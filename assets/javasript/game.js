d5dbf29d738fd5f9f2c9850b32790aa5c075c974
row_gregory <rowgregory@gmail.com>
2018-11-16 17:57:23 -0500

    formatted document

--- assets/javasript/game.js
+++ assets/javasript/game.js
@@ -1,181 +1,71 @@
-// Global variables
-// =================================================
-
-
 let wins = 0;
 let losses = 0;
 let guessesLeft = 10;
 let yourGuess = []; 
 let computerChoice = '';
 
-
-
 const game = () => {
 
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    let computerGuess = alphabet[Math.floor(Math.random() * 26)];
-    let computerChoice = computerGuess;
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let computerGuess = alphabet[Math.floor(Math.random() * 26)];
+  let computerChoice = computerGuess;
+  console.log(computerChoice);
 
-    console.log(computerChoice);
+  const checkLetter = () => {
 
-    
+    document.onkeyup = (event) => {
 
-    const checkLetter = () => {
+      let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-        document.onkeyup = (event) => {
-
-            let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-            console.log(`This is the letter you guessed: ${userGuess}`);
-
-            if (event.keyCode < 65 || event.keyCode > 90) {
+      if (event.keyCode < 65 || event.keyCode > 90) {
                 
-                alert("Please select a letter!");
+        alert("Please select a letter!");
 
-            }  else if (yourGuess.indexOf(userGuess) >= 0) {
-                console.log('are we here');
+      } else if (yourGuess.indexOf(userGuess) >= 0) {
                 
-                alert('You already guessed that!');
+        alert('You already guessed that!');
 
-            } else if (userGuess === computerChoice) {
+      } else if (userGuess === computerChoice) {
 
-                alert('You Win');
-                
-                wins++;
+        alert('You Win');
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        resetGame();
 
-                document.getElementById("wins").innerHTML = wins;
+      } else {
 
-                resetGame();
+        guessesLeft--;
+        document.getElementById("guessesLeft").innerHTML = guessesLeft;
+        yourGuess.push(userGuess);
+        document.getElementById("guessesSoFar").innerHTML = yourGuess.join(", ");
+        allOutOfGuesses();
+      }
+    } 
+  };
 
-            } else {
+  checkLetter();
 
-                guessesLeft--;
+  const resetGame = () => {
 
-                document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    guessesLeft = 10;
+    yourGuess = [];
+    document.getElementById('guessesLeft').innerHTML = guessesLeft;
+    document.getElementById('guessesSoFar').innerHTML = yourGuess;
+    game();
+  };
 
-                yourGuess.push(userGuess);
+  const allOutOfGuesses = () => {
 
-                document.getElementById("guessesSoFar").innerHTML = yourGuess.join(", ");
+    if(guessesLeft === 0) {
+        alert('You Lose!');
+        losses++;
+        document.getElementById('losses').innerHTML = losses;
+        resetGame();
 
-                allOutOfGuesses();
-            
-            }
-        } 
-    };
-
-    const resetGame = () => {
-
-        guessesLeft = 10;
-        yourGuess = [];
-        document.getElementById('guessesLeft').innerHTML = guessesLeft;
-        document.getElementById('guessesSoFar').innerHTML = yourGuess;
-        game();
-    };
-
-    const allOutOfGuesses = () => {
-
-        if(guessesLeft === 0) {
-            alert('You Lose!');
-            losses++;
-            document.getElementById('losses').innerHTML = losses;
-            resetGame();
-
-        } else {
-            checkLetter();
-        }
-    }  
+    } else {
+        checkLetter();
+    }
+  }  
 };
 
 game();
-
-// // lets the computer select a random letter from the alphabet
-
-// console.log(`This is the computer guess: ${computerGuess}`);
-
-// window.onload = function () {
-//     document.getElementById('wins').innerHTML = `Wins: ${wins}`
-//     document.getElementById('losses').innerHTML = `Losses: ${losses}`
-//     document.getElementById('guessesLeft').innerHTML = `Guesses Left: ${guessesLeft}`
-//     document.getElementById('guessesSoFar').innerHTML = `Guesses so far: ${guessesSoFar}`
-// }
-
-// // user inputs a key to the keyboard
-// document.onkeyup = function (event) {
-
-
-
-//     
-//     console.log(userGuess);
-
-//     
-
-
-
-
-//     if (userGuess === computerGuess) {
-//         win();
-//         alert('You chose the right letter!')
-//         guessesLeft = 10;
-//         guessesSoFar = [];
-//     } else {
-//         lose();
-//     }
-
-//     if (guessesLeft === 0) {
-//         alert("You Lose");
-//         guessLeft();
-//     }
-
-//     if (userGuess != computerGuess) {
-//         guessesSoFar.push(userGuess);
-//         document.getElementById('guessesSoFar').innerHTML = `Guesses so far: ${guessesSoFar.join(', ')}`
-//     }
-
-//     function guessLeft() {
-//         losses++;
-//         resetGame();
-//         document.getElementById('losses').innerHTML = `Losses: ${losses}`
-        
-//     }
-
-//     function win() {
-//         wins++;
-//         document.getElementById('wins').innerHTML = `Wins: ${wins}`
-//         resetGame();
-//     }
-
-//     function lose() {
-//         guessesLeft--;
-//         document.getElementById('guessesLeft').innerHTML = `Guesses Left: ${guessesLeft}`
-//     }
-
-//     function resetGame() {
-//         guessesLeft = 10;
-//         computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-//         console.log(`This is the computer guess: ${computerGuess}`);
-//         guessesSoFar = [];
-//     }
-
-
-
-
-//     // "<p>Losses: " + losses + "</p>" +
-//     // "<p>Guesses Left: " + guessesLeft + "</p>" +
-//     // "<p>Guesses so far: " + guessesSoFar.join(', ') 
-
-
-//     // guessesSoFar.innerHTML = guessesSoFar.join(',');
-//     // document.getElementById("psychicAct").innerHTML = html;  
-// }
-
-
-
-
-
-
-
-// if it is not a match, decrememt 1 from guesses left, display guesses so far
-
-// if guessesLeft = 0, everything gets reset
-
-// if user input matches computer choice then increment wins by 1 and reset game
